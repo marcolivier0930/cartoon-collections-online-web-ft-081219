6fb4d988ffd9b112c6124f8bff0621576cd830e6
Marc Blanchard <marcblanchard0930@gmail.com>
2019-08-26 16:17:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf} "
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer) # code an argument here
   # Your code here
+  planeteer.map {|i| i.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(short_words)
+  # code an argument here
   # Your code here
+  short_words.any?  {|word| word.size > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(have_cheese)
+  # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  
 end
